ce7ec995648bf124f2af714df54c13aa94690e16
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-13 21:42:25 +0100

    Add new Dtos

---
+++ TMAWarehouse.Services.Item/Models/Dto/ItemDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TMAWarehouse.Services.Item.Models.Dto
+{
+    public class ItemDto
+    {
+        public int ItemID { get; set; }
+
+        public string Group { get; set; }
+
+        public string UnitOfMeasurement { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal PriceWithoutVAT { get; set; }
+
+        public string Status { get; set; }
+
+        public string StorageLocation { get; set; }
+
+        public string ContactPerson { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
