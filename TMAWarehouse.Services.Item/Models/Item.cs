493c5a1a40a07ca026fb697cb034041a19ad57b2
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-13 21:16:05 +0100

    Add Item model

---
+++ TMAWarehouse.Services.Item/Models/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TMAWarehouse.Services.Item.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+
+        [Required]
+        public string Group { get; set; }
+
+        [Required]
+        public string UnitOfMeasurement { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal PriceWithoutVAT { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        public string StorageLocation { get; set; }
+
+        public string ContactPerson { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
