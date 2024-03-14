c84d8d4f650a9280886467c7b85755fe7e3e8f97
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-14 21:42:52 +0100

    Add Dtos

---
+++ TMAWarehouse.Web/Models/Dto/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using static TMAWarehouse.Web.Utility.SD;
+
+namespace TMAWarehouse.Web.Models.Dto
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public string Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
