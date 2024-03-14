024813002d7077631565ce4c573bc08641ed5c09
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-14 20:39:17 +0100

    Add Models and Dtos

---
+++ TMAWWarehouse.Services.TMARequests/Models/Dto/TMARequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TMAWWarehouse.Services.TMARequests.Models.Dto
+{
+    public class TMARequestDto
+    {
+        public int RequestID { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Group { get; set; }
+
+        public string UnitOfMeasurement { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal PriceWithoutVAT { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ TMAWWarehouse.Services.TMARequests/Models/TMARequest.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TMAWWarehouse.Services.TMARequests.Models
+{
+    public class TMARequest
+    {
+        [Key]
+        public int RequestID { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        public string Group { get; set; }
+
+        [Required]
+        public string UnitOfMeasurement { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal PriceWithoutVAT { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Status { get; set; }
+    }
+}
