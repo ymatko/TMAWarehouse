a1251520bfd510ccfe30062176590b6dea4c4f14
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-14 21:40:33 +0100

    Create TMAWarehouse.Web

---
+++ TMAWarehouse.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TMAWarehouse.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
