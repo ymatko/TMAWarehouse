6db48487da5f46949ba52ad7f593407757221fbb
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-15 10:22:16 +0100

    Add ItemController to Web

---
+++ TMAWarehouse.Web/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TMAWarehouse.Web.Models.Dto;
+using TMAWarehouse.Web.Services.IServices;
+
+namespace TMAWarehouse.Web.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly IItemService _itemService;
+        public ItemController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+        public async Task<IActionResult> ItemIndex()
+        {
+            IEnumerable<ItemDto>? list = Enumerable.Empty<ItemDto>();
+            ResponseDto? response = await _itemService.GetAllItemsAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<IEnumerable<ItemDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
