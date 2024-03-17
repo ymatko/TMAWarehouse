ccde18df5b8d65851a58e72ee72cb6d2d2aae76e
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-17 22:14:19 +0100

    Add Dtos

---
+++ TMAWarehouse.Service.Auth/Models/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TMAWarehouse.Service.Auth.Models.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TMAWarehouse.Service.Auth/Models/Dto/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TMAWarehouse.Service.Auth.Models.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
