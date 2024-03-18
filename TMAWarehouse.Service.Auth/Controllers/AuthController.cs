1a8dcf4db1c37a1435e208815344063b4b0a1774
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-18 11:39:27 +0100

    Add Role to Auth

--- TMAWarehouse.Service.Auth/Controllers/AuthController.cs
+++ TMAWarehouse.Service.Auth/Controllers/AuthController.cs
@@ -22,7 +22,7 @@ namespace TMAWarehouse.Service.Auth.Controllers
         public async Task<IActionResult> Register([FromBody] RegistrationRequestDto model)
         {
             var errorMessage = await _authService.Register(model);
-            if(!string.IsNullOrEmpty(errorMessage))
+            if (!string.IsNullOrEmpty(errorMessage))
             {
                 _response.IsSuccess = false;
                 _response.Message = errorMessage;
@@ -35,13 +35,26 @@ namespace TMAWarehouse.Service.Auth.Controllers
         public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
         {
             var loginResponse = await _authService.Login(model);
-            if(loginResponse.User == null)
+            if (loginResponse.User == null)
             {
                 _response.IsSuccess = false;
                 _response.Message = "Username or password is incorrect";
                 return BadRequest(_response);
             }
             _response.Result = loginResponse;
+            return Ok(_response);
+        }
+
+        [HttpPost("AssignRole")]
+        public async Task<IActionResult> AssignRole([FromBody] RegistrationRequestDto model)
+        {
+            var assignRoleSuccessful = await _authService.AssignRole(model.Email, model.Role.ToUpper());
+            if (!assignRoleSuccessful)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "Error encountered";
+                return BadRequest(_response);
+            }
             return Ok(_response);
         }
     }
--- TMAWarehouse.Service.Auth/Service/IService/IAuthService.cs
+++ TMAWarehouse.Service.Auth/Service/IService/IAuthService.cs
@@ -6,5 +6,6 @@ namespace TMAWarehouse.Service.Auth.Service.IService
     {
         Task<string> Register(RegistrationRequestDto registrationRequestDto);
         Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<bool> AssignRole(string email, string roleName);
     }
 }
