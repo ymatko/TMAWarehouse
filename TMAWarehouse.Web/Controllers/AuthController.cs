d429d54de8827fd43c419bb44169994bd7631515
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-18 12:55:42 +0100

    Add AuthController and View Login & Register

---
+++ TMAWarehouse.Web/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using TMAWarehouse.Web.Models.Dto;
+using TMAWarehouse.Web.Services.IServices;
+
+namespace TMAWarehouse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
