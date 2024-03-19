0cdc99c5c08a83200eb7f5da676bdf6aac18807e
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-19 20:11:16 +0100

    Add Dto

---
+++ TMAWarehouse.Web/Models/Dto/SetRoleRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TMAWarehouse.Web.Models.Dto
+{
+	public class SetRoleRequestDto
+	{
+		public IdentityUser User { get; set; }
+		public string RoleName { get; set; }
+	}
+}
