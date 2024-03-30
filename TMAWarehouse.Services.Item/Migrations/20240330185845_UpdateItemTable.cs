4a48b45edbd3cab75b77644ed843a24c2bd56cdf
Yevhen Matko <n246v1j80x6t@gmail.com>
2024-03-30 19:59:44 +0100

    Update Item Table in Db

---
+++ TMAWarehouse.Services.Item/Migrations/20240330185845_UpdateItemTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TMAWarehouse.Services.Item.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Photo",
+                table: "Items",
+                newName: "PhotoUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoLocalPach",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoLocalPach",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "PhotoUrl",
+                table: "Items",
+                newName: "Photo");
+        }
+    }
+}
